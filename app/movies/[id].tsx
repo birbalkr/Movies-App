82d457a56518c85e56696c315282d395e16c5c02
birbalkr <birbalkr1435@gmail.com>
2025-05-03 07:08:53 +0530

    home ui

---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const details = () => {
+    return (
+        <View>
+            <Text>details</Text>
+        </View>
+    )
+}
+
+export default details
